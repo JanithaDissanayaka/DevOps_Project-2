1087b3c88265079f89ba5ceedab0543647eeb661
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-03-17 17:13:45 +0530

    change version

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,15 +1,18 @@
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.car-sale.endpoint
   token                  = data.aws_eks_cluster_auth.car-sale.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.car-sale.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.app.certificate_authority[0].data)
 }
-
+/* fro check it work*/
 data "aws_eks_cluster" "car-sale" {
   name = module.eks.cluster_name
+  depends_on = [ module.eks ]
 }
 
 data "aws_eks_cluster_auth" "car-sale" {
   name = module.eks.cluster_name
+  depends_on = [ module.eks ]
 }
 
 module "eks" {
@@ -17,59 +20,50 @@ module "eks" {
   version = "~> 21.0"
 
   name               = var.cluster_name
-  kubernetes_version = "1.33"   # ✅ stable version
+  kubernetes_version = "1.33"
 
-  enable_irsa = true            # ✅ REQUIRED
-
-  ################################
-  # ADDONS
-  ################################
   addons = {
-    coredns = {}
+  coredns = {}
 
-    eks-pod-identity-agent = {
-      before_compute = true
-    }
-
-    kube-proxy = {}
-
-    vpc-cni = {
-      before_compute = true
-    }
-
-    aws-ebs-csi-driver = {
-      most_recent              = true
-      service_account_role_arn = aws_iam_role.ebs_csi_driver.arn  # ✅ REQUIRED
-    }
+  eks-pod-identity-agent = {
+    before_compute = true
   }
 
-  ################################
-  # NETWORK
-  ################################
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  kube-proxy = {}
 
-  ################################
-  # NODE GROUP
-  ################################
-  eks_managed_node_groups = {
+  vpc-cni = {
+    before_compute = true
+  }
+
+}
+
+  # Optional
+  endpoint_public_access = true
+
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  
+
+  
+    eks_managed_node_groups = {
     example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.small"
-
-      ]
+      instance_types = ["t3.small"]
 
       min_size     = 2
       max_size     = 5
-      desired_size = 3
+      desired_size = 4
     }
   }
-
-  endpoint_public_access = true
-  enable_cluster_creator_admin_permissions = true
 
   tags = {
     Environment = "prod"
     Terraform   = "true"
   }
 }
+####
